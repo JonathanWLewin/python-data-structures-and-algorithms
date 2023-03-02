91f476efeb98ad0e97c0870080a9af08bf8a3c21
jonathan.lewin@kochind.com <jonathan.lewin@kochind.com>
2023-03-02 16:47:13 -0600

    Special templating for stack

--- pyrefine/StackImplementer.py
+++ pyrefine/StackImplementer.py
@@ -9,10 +9,19 @@ class StackImplementer(BaseObject):
     _title = "Stack"
     def __init__(self) -> None:
         super().__init__()
-        self.stack = Stack()
     
     def generate_examples(self):
         examples = [
-            Example('Initialization', 'Initialization of empty stack', 2)
+            Example('Initialization', 'Initialization of empty stack', 2, 'stack = Stack()', reversed(self.example_1())),
+            Example('Push', 'Pushing a value to the top of the stack', 18, 'stack.push(15)', reversed(self.example_2()))
         ]
         return examples
+    
+    def example_1(self):
+        stack = Stack()
+        return stack.stack
+    
+    def example_2(self):
+        stack = Stack()
+        stack.push(2)
+        return stack.stack
