f0c95bc750eae1feb713a2e24d913930a8a80441
jonathan.lewin@kochind.com <jonathan.lewin@kochind.com>
2023-03-10 16:45:29 -0600

    Added tests

--- pyrefine/dataStructures.py
+++ pyrefine/dataStructures.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 from pyrefine.StackImplementer import StackImplementer
 
 bp = Blueprint("data-structures", __name__, url_prefix="/data-structures")
@@ -7,6 +7,8 @@ bp = Blueprint("data-structures", __name__, url_prefix="/data-structures")
 def stack(id):
     stack = StackImplementer()
     title, description, code, examples = stack.get_template_values()
-    example = examples[id - 1]
     length = len(examples)
+    if id < 1 or id > length:
+        abort(404)
+    example = examples[id - 1]
     return render_template("dataStructures/stack.html", title=title, description=description, code=code, example=example, length=length, id=id)
