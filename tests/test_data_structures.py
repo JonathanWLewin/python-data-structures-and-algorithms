b917dd8637480005f24e878a862c1c5d4d464a1a
JonathanLewin <jonathanlewin@hotmail.co.uk>
2023-03-11 19:31:24 -0600

    auto scroll to examples

--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -1,7 +1,9 @@
 from pyrefine.StackImplementer import StackImplementer
 
-def test_stack(client):
+def test_stack_nav_buttons(client):
     stack = StackImplementer()
-    stack.generate_examples
-    response = client.get('/data-structures/stack/example/1')
-    assert b'<input type="submit" value="Previous" class="disabled">' in response.data
+    examples = stack.generate_examples()
+    response_1 = client.get('/data-structures/stack/example/1')
+    assert b'<input type="submit" value="Previous" class="disabled">' in response_1.data
+    response_2 = client.get(f'/data-structures/stack/example/{len(examples)}')
+    assert b'<input type="submit" value="Next" class="disabled">' in response_2.data
