67c84b90aac172bd5756b85de18df5369fd26346
JonathanLewin <jonathanlewin@hotmail.co.uk>
2023-03-15 00:10:50 -0500

    Added some more tests

--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -1,9 +1,31 @@
 from pyrefine.StackImplementer import StackImplementer
 
-def test_stack_nav_buttons(client):
+def test_nav_buttons(client):
     stack = StackImplementer()
     examples = stack.generate_examples()
     response_1 = client.get('/data-structures/stack/example/1')
     assert b'<input type="submit" value="Previous" class="disabled">' in response_1.data
     response_2 = client.get(f'/data-structures/stack/example/{len(examples)}')
     assert b'<input type="submit" value="Next" class="disabled">' in response_2.data
+
+def test_stack_examples(client):
+    stack = StackImplementer()
+    examples = stack.generate_examples()
+
+    for i in range(len(examples)):
+        response = client.get(f'/data-structures/stack/example/{i+1}')
+        example = examples[i]
+        assert bytes(f'<span class="pre-wrap">{example.description}</span>', encoding='utf8') in response.data
+        assert bytes(f'<h3>{example.title}</h3>', encoding='utf-8') in response.data
+        assert bytes(f'<span>{example.input | safe}</span>')
+
+        output = example.output
+        state = example.state
+        if output != '':
+            assert b"output" in response.data
+            assert bytes(f'<span class="pre-wrap">{output}</span>', encoding='utf8') in response.data
+        else:
+            assert b"output" not in response.data
+
+        for value in state:
+            assert bytes(f'<span class="box-value">{value}</span>', encoding='utf8') in response.data
