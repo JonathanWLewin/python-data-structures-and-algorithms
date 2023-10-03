b6d6f15d6cae9fe119f98b03dc97c0690c28535d
JonathanWLewin <jonathanwlewin@gmail.com>
2023-10-03 00:13:00 -0500

    Created the doubly linked list

--- pyrefine/DataStructures/DataStructures.py
+++ pyrefine/DataStructures/DataStructures.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, abort
 from pyrefine.DataStructures.Stack.StackImplementer import StackImplementer
 from pyrefine.DataStructures.LinkedList.LinkedListImplementor import LinkedListImplementor
+from pyrefine.DataStructures.DoublyLinkedList.DoublyLinkedListImplementor import DoublyLinkedListImplementor
 
 bp = Blueprint("data-structures", __name__, url_prefix="/data-structures")
 
@@ -23,3 +24,13 @@ def linked_list(id):
         abort(404)
     example = examples[id - 1]
     return render_template("dataStructures/linkedList.html", title=title, code=code, example=example, length=length, id=id)
+
+@bp.route("doublelinkedlist/example/<int:id>")
+def double_linked_list(id):
+    lst = DoublyLinkedListImplementor()
+    title, code, examples = lst.get_template_values()
+    length = len(examples)
+    if id < 1 or id > length:
+        abort(404)
+    example = examples[id - 1]
+    return render_template("dataStructures/doublyLinkedList.html", title=title, code=code, example=example, length=length, id=id)
